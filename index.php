b989c5533e81ba5ef503111779ddd587834c99bf
Jheng <a0960073588@gmail.com>
2023-01-05 15:00:03 +0800

    新增404.php

--- index.php
+++ index.php
@@ -49,6 +49,11 @@ $routes =
 
 ];
 
+//var_dump($routes[$uri]);
 if (array_key_exists($uri, $routes)){
     require $routes[$uri];
+}else{
+    http_response_code(404);
+    require "views/404.php";
+    die();
 }
---
+++ views/404.php
@@ -0,0 +1,15 @@
+<?php require 'partials/head.php'; ?>
+<?php require 'partials/nav.php'; ?>
+
+
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <!-- Replace with your content -->
+      <H1 class="text-2xl fint-bold">Sorry , page not found</H1>
+        <p class="mt-4">
+         <a href="/" class="text-blue underline" >點擊我回首頁</a>
+
+        </p>
+    </div>
+  </main>
+  <?php require 'partials/footer.php'; ?>
