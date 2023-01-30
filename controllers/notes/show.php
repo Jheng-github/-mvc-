9342428f97aa2c3976fe511b59f50c6123ac5b02
Jheng <a0960073588@gmail.com>
2023-01-30 12:18:49 +0800

    修復show.php無法正常顯示單筆留言

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -6,7 +6,9 @@ use core\Database;
 $config = require base_path('config.php');
 $db = new Database($config['database']); //在這邊產生根資料庫連地__connection , 
 //dd($db); //確認有東西
-$currentUserId =1;
+//$currentUserId =1;
+
+
 // $heading = "Note";
 $id =$_GET['id']; // foreach note['id']
 //dd($id);
@@ -23,9 +25,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){ //提交刪除表單
     header('location: /notes');
     exit();
 }else{
-// //dd($note); //確認有取得資料
+//dd($note); //確認有取得資料
 $note = $db->query('select * from notes where id = :id',['id'=>$id ]) -> findOrFail();
-authorize($note['user_id'] == $currentUserId);  // 包裝下方
+authorize($note['user_id'] == $_SESSION['user_id']);  // 包裝下方
 view("notes/show.view.php",[
     'heading' => 'Note',
     'note' => $note
